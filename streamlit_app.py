fe769150219c8c83484a4e9bd19197ef6d472b3e
Yash Saxena <30576909+Yashrsaxena@users.noreply.github.com>
2023-01-23 12:41:46 +0530

    Update streamlit_app.py
    Updated the title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents\' new healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
