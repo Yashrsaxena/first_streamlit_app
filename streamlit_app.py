f000ff55ddea34fdd4ad097a7c616ffbf1e35162
Yash Saxena <30576909+Yashrsaxena@users.noreply.github.com>
2023-01-24 00:28:27 +0530

    Fixed display message

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ try:
   if not add_my_fruit:
     st.error("No fruit to Add")
   else:
-    st.write("Thank you for adding the fruit "+insert_row_snowflake(add_my_fruit))
+    st.write(insert_row_snowflake(add_my_fruit))
 
 except URLError as e:
   st.error()
